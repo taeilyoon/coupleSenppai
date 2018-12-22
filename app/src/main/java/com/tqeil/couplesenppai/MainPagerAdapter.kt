82ceb07e660f0ab7edae85020ffb8d14ce7828a9
jspiner <jspiner@naver.com>
2018-12-22 16:50:05 +0900

    해당하는 position의 fragment를 생성

--- app/src/main/java/com/tqeil/couplesenppai/MainPagerAdapter.kt
+++ app/src/main/java/com/tqeil/couplesenppai/MainPagerAdapter.kt
@@ -3,6 +3,7 @@ package com.tqeil.couplesenppai
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import java.lang.IllegalArgumentException
 
 class MainPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
 
@@ -13,7 +14,13 @@ class MainPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAda
     override fun getCount() = PAGE_COUNT
 
     override fun getItem(position: Int): Fragment {
-        TODO("not implemented")
+        return when(position) {
+            0 -> CalendarFragment.create()
+            1 -> ChatFragment.create()
+            2 -> FeedFragment.create()
+            3 -> MatchFragment.create()
+            else -> throw IllegalArgumentException("올바르지 못한 position : $position")
+        }
     }
 
 }
