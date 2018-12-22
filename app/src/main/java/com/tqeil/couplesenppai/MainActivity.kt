7d6b7c6fef1e70b00a45cefe7807e55e3e179048
jspiner <jspiner@naver.com>
2018-12-22 16:58:02 +0900

    페이지에 맞게 navigation id 변경

--- app/src/main/java/com/tqeil/couplesenppai/MainActivity.kt
+++ app/src/main/java/com/tqeil/couplesenppai/MainActivity.kt
@@ -59,22 +59,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_calendar -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_chat -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_feed -> {
 
             }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.nav_match -> {
 
             }
         }
