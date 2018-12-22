003dc122bf728ee189f5c0bfeff0e2b7f4f6effb
yuntaeil <zkrxpdlf0808@gmail.com>
2018-12-22 16:55:27 +0900

    Firebase installed

---
+++ app/src/main/java/com/tqeil/couplesenppai/ApplicationClass.kt
@@ -0,0 +1,18 @@
+package com.tqeil.couplesenppai
+
+import android.app.Application
+import android.content.Context
+import java.util.function.Consumer
+
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+    companion object {
+
+    }
+}
